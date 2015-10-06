ec0eb1e4069d87c6814362745243e8007fac0173
unyoungwax <unyoungwax@hotmail.com>
2015-10-06 13:40:46 +0800

    Integrated UI with Logic

--- GetStuffDone/src/UI.java
+++ GetStuffDone/src/UI.java
@@ -14,17 +14,18 @@ public class UI {
 
 	private static final String TITLE = "GetStuffDone";
 
-	private static final String START = "    START\t";
-	private static final String END = "    END\t";
-	private static final String VENUE = "    VENUE\t";
-	private static final String PRIORITY = "    PRIORITY\t";
-
 	private JFrame frame = new JFrame(TITLE);
 	private TextView displayBox = new TextView();
 	private TextView feedbackBox = new TextView();
 	private JTextField commandBar = new JTextField();
 
+	public static void main(String[] args) {
+		new UI();
+	}
+
 	public UI() {
+
+		Logic logic = new Logic();
 
 		// Initialize the main window
 		frame.setLayout(new BorderLayout());
@@ -41,14 +42,16 @@ public class UI {
 
 			@Override
 			public void actionPerformed(ActionEvent event) {
-				/**
-				 * TODO process input
-				 * 
-				 * Ideal code:
-				 * 
-				 * String input = commandBar.getText();
-				 * logic.processInput(input);
-				 */
+
+				String input = commandBar.getText();
+				commandBar.setText("");
+
+				Feedback feedback = logic.processInput(input);
+
+				feedbackBox.display(input);
+
+				showInFeedbackBox(feedback.getFeedbackString());
+				showInDisplayBox(feedback.getDisplayString());
 			}
 		});
 
@@ -62,62 +65,18 @@ public class UI {
 		frame.setVisible(true);
 	}
 
-	/**
-	 * Display the feedback in the next line in the feedback box
-	 * 
-	 * @param feedback
-	 *            is the string to be displayed
-	 */
-	public void displayFeedback(String feedback) {
+	private void showInFeedbackBox(String string) {
 
-		if (isValidString(feedback)) {
-			feedbackBox.display(feedback);
+		if (isValidString(string)) {
+			feedbackBox.display(string);
 		}
 	}
 
-	/**
-	 * Display the specified list of tasks in the display box
-	 * 
-	 * @param tasks
-	 *            is the ArrayList of Tasks
-	 */
-	public void displayTasks(ArrayList<Task> tasks) {
+	private void showInDisplayBox(String string) {
 
-		if (tasks.isEmpty() || tasks == null) {
-			return;
-		}
-
-		displayBox.clearText();
-
-		for (Task task : tasks) {
-			displayTask(task);
-		}
-	}
-
-	private void displayTask(Task task) {
-
-		assert(task != null);
-
-		if (isValidString(task.getDescription())) {
-			displayBox.display(task.getDescription());
-		} else {
-			return;
-		}
-
-		if (task.getStartDate() != null) {
-			displayBox.display(START + task.getStartDate().toString());
-		}
-
-		if (task.getdeadline() != null) {
-			displayBox.display(END + task.getdeadline().toString());
-		}
-
-		if (isValidString(task.getVenue())) {
-			displayBox.display(VENUE + task.getVenue());
-		}
-
-		if (isValidString(task.getPriority())) {
-			displayBox.display(PRIORITY + task.getPriority());
+		if (isValidString(string)) {
+			displayBox.clearText();
+			displayBox.display(string);
 		}
 	}
 
