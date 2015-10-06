25586d368710212a192dbb031539b6e8f19ab583
chewwwwwwwwww <chewwwwwwwwww@hotmail.com>
2015-10-06 14:16:12 +0800

    processinput feedback touch up

--- GetStuffDone/src/Logic.java
+++ GetStuffDone/src/Logic.java
@@ -1,6 +1,5 @@
 import java.util.*;
 
-import Parser.COMMANDS;
 
 /*
  * Logic deals with handling of input commands, CRUD of tasks, update of History and update of Storage
@@ -12,7 +11,11 @@ public class Logic {
 
 	private static final String TASK_NOT_FOUND = "Task was not found";
 	private static final String DEFAULT_FILE_NAME = "mytextfile.txt";
-	public static ArrayList<Task> tasks = new ArrayList<Task>();
+	private static final String FEEDBACK_ADD = "ADDED ";
+	private static final String FEEDBACK_SEARCH = "";
+	private static final String FEEDBACK_UPDATE = "";
+	private static final String FEEDBACK_DELETE = "";
+	public ArrayList<Task> tasks = new ArrayList<Task>();
 	public static ArrayList<Integer> tasksInAction = new ArrayList<Integer>();
 	public CommandDetails commandDetails;
 	private Scanner sc;
@@ -20,18 +23,19 @@ public class Logic {
 	private Storage storage = new Storage(DEFAULT_FILE_NAME);
 	private History history = new History();
 	
-	public String processInput(String input)	{
+	public Feedback processInput(String input)	{
 		this.commandDetails = Parser.parse(input);
+		Feedback feedback;
 		switch (this.commandDetails.getCommand()) {
 		case ADD:
-			String displayText = createTask();
-			Feedback feedback = new Feedback(displayText, ADD_TEXT_FEEDBACK)
+			return feedback = new Feedback(createTask(), FEEDBACK_ADD + commandDetails.getDescription());
+			break;
 		case DELETE:
 			deleteTask();
 			
 		case SEARCH:
-			readTask();
-		
+			return feedback = new Feedback(searchTask(), FEEDBACK_SEARCH + commandDetails.getDescription());
+			break;
 		case DONE:
 			
 		case HELP:
@@ -41,9 +45,6 @@ public class Logic {
 		case UNDO:
 			
 		}
-
-		
-		Feedback feedback = new Feedback();
 	}
 
 	//Constructor
@@ -57,32 +58,23 @@ public class Logic {
 	private String createTask()	{
 		Task task = new Task(this.commandDetails);
 		tasks.add(task);
-		task.toString();
+		return tasks.size() + " " + task.toString();
 	}
 
 	//Print task according to the given commandDetails
-	private void readTask()	{
+	private String searchTask()	{
+		String display=null;
 
-		this.searchTask();
-
-		if(tasksInAction.isEmpty())	{	
-			System.out.println(TASK_NOT_FOUND);
-		}
-		else	{
-			for(int i = 0; i < tasksInAction.size(); i++)	{
-				tasks.get(tasksInAction.get(i)).toString();
-			}
-		}
-		tasksInAction.clear();
-	}
-
-	//Records the index of found tasks in tasksInAction
-	private void searchTask()	{
 		for(int i = 0; i < tasks.size(); i++)	{
 			if(tasks.get(i).contains(commandDetails))	{
-				tasksInAction.add(i);
+				display.concat(i + tasks.get(i).toString());
 			}
 		}
+
+		if(display == null)	{	
+			display = TASK_NOT_FOUND;
+		}
+		return display;
 	}
 
 	private void updateTask()	{
