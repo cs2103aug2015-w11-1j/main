a8d22fe0beb6852dd2fc1906569d87bc0b3a18e1
chewwwwwwwwww <chewwwwwwwwww@hotmail.com>
2015-10-06 13:17:44 +0800

    create storage object

---
+++ GetStuffDone/src/Storage.java
@@ -0,0 +1,25 @@
+
+public class Storage {
+	
+	public Storage(String fileName)	{
+		//create file
+	}
+	
+	public void save(ArrayList<Task> tasks)	{
+		
+	}
+	
+	public ArrayList<Task> load(){
+		ArrayList<Task> tasks = new ArrayList<Task();
+		while(file has nextline)	{
+				Task task = new task();
+				task.setDescription() = sc.nextLine();
+				task.setStartDate() = sc.nextLine();
+				task.setDeadline() = sc.nextLine();
+				task.setVenue() = sc.nextLine();
+				task.setPriority() = sc.nextLine();
+				tasks.add(task);
+		}
+	}
+
+}
