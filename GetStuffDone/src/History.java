d416ab8a8779c2ccc2ec2b8e077cca651edaabfd
renfc145 <renfc145@gmail.com>
2015-10-15 00:36:32 +0800

    Updated history for update command
    Updated history to support the update command. Changed undo() and redo()
    methods.

--- GetStuffDone/src/History.java
+++ GetStuffDone/src/History.java
@@ -1,10 +1,10 @@
 import java.util.Stack;
 
 public class History {
-	
+
 	private Stack<CommandDetails> undoStack = new Stack<CommandDetails>();
 	private Stack<CommandDetails> redoStack = new Stack<CommandDetails>();
-	
+
 	//constructor
 	public History () {
 		this.undoStack = new Stack<CommandDetails>();
@@ -20,31 +20,50 @@ public class History {
 			redoStack.clear();
 			return 1;
 		}
-		
+
 		catch (Exception e) {
 			return 0;
 		}
 	}
-	
+
 	//this method returns the last action, returns null of undoStack is empty
 	public CommandDetails undo() {
 		if (undoStack.isEmpty()) {
 			return null;
 		} else {
 			CommandDetails temp = undoStack.pop();
-			redoStack.push(temp);
-			return temp;
+			switch (temp.getCommand()) {
+			case UPDATE:
+				CommandDetails temp2 = undoStack.pop();
+				redoStack.push(temp);
+				redoStack.push(temp2);
+				return temp;
+
+			default: 
+				redoStack.push(temp);
+				return temp;
+
+			}
 		}
 	}
-	
+
 	//this method returns the last action undid, returns null of redoStack is empty
 	public CommandDetails redo() {
 		if (redoStack.isEmpty()) {
 			return null;
 		} else {
 			CommandDetails temp = redoStack.pop();
-			undoStack.push(temp);
-			return temp;
+			switch (temp.getCommand()) {
+			case UPDATE:
+				CommandDetails temp2 = redoStack.pop();
+				undoStack.push(temp);
+				undoStack.push(temp2);
+				return temp;
+
+			default: 
+				undoStack.push(temp);
+				return temp;
+			}
 		}
 	}
 }
