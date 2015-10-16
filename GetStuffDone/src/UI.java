500dd71ce1b56fb60a061bdccad8966e9d1efe8d
unyoungwax <unyoungwax@hotmail.com>
2015-10-16 21:07:38 +0800

    Added Info Box and improved GUI

--- GetStuffDone/src/UI.java
+++ GetStuffDone/src/UI.java
@@ -1,9 +1,12 @@
 import java.awt.BorderLayout;
+import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.ArrayList;
 
+import javax.swing.BoxLayout;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextField;
 
@@ -14,9 +17,16 @@ public class UI {
 
 	private static final String TITLE = "GetStuffDone";
 
-	private JFrame frame = new JFrame(TITLE);
-	private TextView displayBox = new TextView();
-	private TextView feedbackBox = new TextView();
+	private static final int DISPLAY_BOX_WIDTH = 400;
+	private static final int DISPLAY_BOX_HEIGHT = 400;
+	private static final int FEEDBACK_BOX_WIDTH = 200;	
+	private static final int FEEDBACK_BOX_HEIGHT = 300;
+	private static final int INFO_BOX_WIDTH = 200;
+	private static final int INFO_BOX_HEIGHT = 100;
+
+	private TextView displayBox = new TextView(DISPLAY_BOX_WIDTH, DISPLAY_BOX_HEIGHT);
+	private TextView feedbackBox = new TextView(FEEDBACK_BOX_WIDTH, FEEDBACK_BOX_HEIGHT);
+	private JLabel infoBox = new JLabel();
 	private JTextField commandBar = new JTextField();
 
 	public static void main(String[] args) {
@@ -28,13 +38,23 @@ public class UI {
 		GSDControl gsd = new GSDControl();
 
 		// Initialize the main window
+		JFrame frame = new JFrame(TITLE);
+
 		frame.setLayout(new BorderLayout());
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setResizable(false);
 
 		// Attach the UI componenets
+		JPanel panel = new JPanel();
+		panel.setLayout(new BoxLayout(panel, BoxLayout.PAGE_AXIS));
+
+		infoBox.setPreferredSize(new Dimension(INFO_BOX_WIDTH, INFO_BOX_HEIGHT));
+
+		panel.add(new JScrollPane(feedbackBox));
+		panel.add(infoBox);
+
 		frame.add(new JScrollPane(displayBox), BorderLayout.LINE_START);
-		frame.add(new JScrollPane(feedbackBox), BorderLayout.LINE_END);
+		frame.add(panel, BorderLayout.LINE_END);
 		frame.add(commandBar, BorderLayout.PAGE_END);
 
 		// Process the input when Enter is pressed
@@ -43,15 +63,15 @@ public class UI {
 			@Override
 			public void actionPerformed(ActionEvent event) {
 
-				String input = commandBar.getText();
-				commandBar.setText("");
-
-				Feedback feedback = gsd.processInput(input);
-
-				feedbackBox.display(input);
-
-				showInFeedbackBox(feedback.getFeedbackString());
-				showInDisplayBox(feedback.getDisplayString());
+				 String input = commandBar.getText();
+				 commandBar.setText("");
+				 
+				 showInFeedbackBox(input + "\n", TextView.STYLE_NORMAL);
+				
+				 Feedback feedback = gsd.processInput(input);
+				 
+				 showInFeedbackBox(feedback.getFeedbackString(), TextView.STYLE_BOLD);
+				 showInDisplayBox(feedback.getDisplayString(), TextView.STYLE_NORMAL);
 			}
 		});
 
@@ -65,18 +85,25 @@ public class UI {
 		frame.setVisible(true);
 	}
 
-	private void showInFeedbackBox(String string) {
+	private void showInFeedbackBox(String string, String style) {
 
 		if (isValidString(string)) {
-			feedbackBox.display(string);
+			feedbackBox.display(string, style);
 		}
 	}
 
-	private void showInDisplayBox(String string) {
+	private void showInDisplayBox(String string, String style) {
 
 		if (isValidString(string)) {
-			displayBox.clearText();
-			displayBox.display(string);
+			displayBox.clear();
+			displayBox.display(string, style);
+		}
+	}
+	
+	private void showInInfoBox(String string){
+		
+		if(isValidString(string)) {
+			infoBox.setText(string);
 		}
 	}
 
