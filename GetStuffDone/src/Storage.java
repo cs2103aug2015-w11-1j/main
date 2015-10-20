49bf622e7c6e9fcece7a964c8ee580b2f83500e8
unyoungwax <unyoungwax@hotmail.com>
2015-10-20 16:12:19 +0800

    Storage - Implemented setPath() and added tests

--- GetStuffDone/src/Storage.java
+++ GetStuffDone/src/Storage.java
@@ -3,6 +3,9 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.nio.file.Files;
+import java.nio.file.InvalidPathException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -11,25 +14,29 @@ import java.util.List;
 
 public class Storage {
 
-	private static final String DEFAULT_FILENAME = "GetStuffDone.sav";
-
-	// Indicate that an attribute of a Task is null
-	private static final String NULL_FIELD = "\\";
+	private static final String DEFAULT_PATH = "GetStuffDone.sav";
 
 	private static final String DATE_FORMAT = "dd MMMM yy HH:mm:ss";
 
+	private static final String STATUS_COMPLETED = "Completed";
+	private static final String STATUS_NOT_COMPLETED = "Not completed";
+
 	// Indicate the number of lines required to store a task
-	private static final int LINES_PER_TASK = 5;
+	private static final int LINES_PER_TASK = 4;
 
 	// Indicate the relative positions of the attributes of a Task
 	private static final int OFFSET_DESCRIPTION = 0;
 	private static final int OFFSET_DATE_START = 1;
 	private static final int OFFSET_DATE_END = 2;
+	private static final int OFFSET_STATUS = 3;
 
 	private SimpleDateFormat formatter = new SimpleDateFormat(DATE_FORMAT);
 
+	private static Path path = (new File(DEFAULT_PATH)).toPath();
+
 	/**
-	 * Save the ArrayList of Task(s) into a file
+	 * Save the ArrayList of Task(s) into a file. If the ArrayList is null, do
+	 * nothing. If the ArrayList is empty, create an empty file.
 	 * 
 	 * @param tasks
 	 *            is the ArrayList of Task(s)
@@ -42,12 +49,13 @@ public class Storage {
 
 		try {
 
-			PrintWriter writer = new PrintWriter(new File(DEFAULT_FILENAME));
+			PrintWriter writer = new PrintWriter(path.toFile());
 
 			for (Task task : tasks) {
 				writeString(writer, task.getDescription());
 				writeDate(writer, task.getStartDate());
 				writeDate(writer, task.getDeadline());
+				writeStatus(writer, task.getIsComplete());
 			}
 
 			writer.close();
@@ -70,7 +78,7 @@ public class Storage {
 	 */
 	public ArrayList<Task> load() throws IOException, ParseException {
 
-		File savedFile = new File(DEFAULT_FILENAME);
+		File savedFile = path.toFile();
 
 		List<String> lines = Files.readAllLines(savedFile.toPath());
 
@@ -80,23 +88,64 @@ public class Storage {
 			return null;
 		}
 
-		for (int i = 0; i < lines.size() / LINES_PER_TASK; i++) {
+		for (int i = 0; i < lines.size(); i += LINES_PER_TASK) {
 
 			Task task = new Task();
 
-			task.setDescription(parseString(lines.get(i * LINES_PER_TASK + OFFSET_DESCRIPTION)));
-			task.setStartDate(parseDate(lines.get(i * LINES_PER_TASK + OFFSET_DATE_START)));
-			task.setDeadline(parseDate(lines.get(i * LINES_PER_TASK + OFFSET_DATE_END)));
+			task.setDescription(parseString(lines.get(i + OFFSET_DESCRIPTION)));
+			task.setStartDate(parseDate(lines.get(i + OFFSET_DATE_START)));
+			task.setDeadline(parseDate(lines.get(i + OFFSET_DATE_END)));
+			task.setIsComplete(parseStatus(lines.get(i + OFFSET_STATUS)));
+
 			tasks.add(task);
 		}
 
 		return tasks;
 	}
 
+	/**
+	 * Set the path of the save file.
+	 * 
+	 * @param string
+	 *            is the string indicating the complete path
+	 * @return true if the path is valid and successfully set, else false
+	 */
+	public boolean setPath(String string) {
+
+		if (string == null || string.isEmpty()) {
+			return false;
+		}
+
+		int lastIndexOfSeparator = string.lastIndexOf(File.separatorChar);
+		
+		if (lastIndexOfSeparator == -1) {
+			return false;
+		}
+		
+		String folderName = string.substring(0, lastIndexOfSeparator + 1);
+		String fileName = string.replace(folderName, "");
+
+		if (folderName.isEmpty() || fileName.isEmpty()) {
+			return false;
+		}
+		
+		if (!(new File(folderName)).exists()) {
+			return false;
+		}
+
+		try {
+			path = Paths.get(string);
+		} catch (InvalidPathException e) {
+			return false;
+		}
+		
+		return true;
+	}
+
 	private void writeString(PrintWriter writer, String string) {
 
 		if (string == null) {
-			writer.println(NULL_FIELD);
+			writer.println();
 		} else {
 			writer.println(string);
 		}
@@ -105,15 +154,24 @@ public class Storage {
 	private void writeDate(PrintWriter writer, Date date) {
 
 		if (date == null) {
-			writer.println(NULL_FIELD);
+			writer.println();
 		} else {
 			writer.println(formatter.format(date));
 		}
 	}
 
+	private void writeStatus(PrintWriter writer, boolean isCompleted) {
+
+		if (isCompleted) {
+			writer.println(STATUS_COMPLETED);
+		} else {
+			writer.println(STATUS_NOT_COMPLETED);
+		}
+	}
+
 	private String parseString(String string) {
 
-		if (string.equals(NULL_FIELD)) {
+		if (string.isEmpty()) {
 			return null;
 		} else {
 			return string;
@@ -122,10 +180,19 @@ public class Storage {
 
 	private Date parseDate(String string) throws ParseException {
 
-		if (string.equals(NULL_FIELD)) {
+		if (string.isEmpty()) {
 			return null;
 		} else {
 			return formatter.parse(string);
+		}
+	}
+
+	private boolean parseStatus(String string) {
+
+		if (string.equalsIgnoreCase(STATUS_COMPLETED)) {
+			return true;
+		} else {
+			return false;
 		}
 	}
 }
---
+++ GetStuffDone/src/StorageTest.java
@@ -0,0 +1,201 @@
+import static org.junit.Assert.*;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.Date;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class StorageTest {
+
+	private static final String PATH_CURRENT = System.getProperty("user.dir") + File.separatorChar;
+	private static final String FILENAME = "saveFile.txt";
+
+	private Storage storage = new Storage();
+	private ArrayList<Task> tasks = null;
+
+	private Task taskFloating = null;
+	private Task taskEvent = null;
+	private Task taskDeadline = null;
+
+	Date date = new Date();
+
+	@Before
+	public void setUp() {
+
+		storage.setPath(PATH_CURRENT + FILENAME);
+
+		tasks = new ArrayList<>();
+
+		// Sample Tasks
+		taskFloating = new Task();
+		taskFloating.setDescription("Floating Task");
+		taskFloating.setIsComplete(false);
+
+		taskEvent = new Task();
+		taskEvent.setDescription("Event Task");
+		taskEvent.setStartDate(new Date());
+		taskEvent.setDeadline(new Date());
+		taskFloating.setIsComplete(true);
+
+		taskDeadline = new Task();
+		taskDeadline.setDescription("Deadline Task");
+		taskDeadline.setDeadline(new Date());
+		taskDeadline.setIsComplete(false);
+	}
+	
+	@Test
+	public void saveNull() throws IOException, ParseException {
+		
+		storage.save(null);
+		
+		assertFalse((new File(PATH_CURRENT + FILENAME)).exists());
+	}
+
+	@Test
+	public void saveEmptyArrayList() throws ParseException, IOException {
+
+		storage.save(new ArrayList<Task>());
+		tasks = storage.load();
+
+		assertEquals(tasks.size(), 0);
+	}
+
+	@Test
+	public void saveNullAttributes() throws IOException, ParseException {
+
+		tasks.add(new Task());
+
+		storage.save(tasks);
+		tasks = storage.load();
+
+		Task task = tasks.remove(0);
+		assertSameTask(task, new Task());
+	}
+
+	@Test
+	public void saveThreeTasks() throws IOException, ParseException {
+
+		tasks.add(taskFloating);
+		tasks.add(taskEvent);
+		tasks.add(taskDeadline);
+
+		storage.save(tasks);
+		tasks = storage.load();
+
+		assertSameTask(taskFloating, tasks.get(0));
+		assertSameTask(taskEvent, tasks.get(1));
+		assertSameTask(taskDeadline, tasks.get(2));
+	}
+
+	@Test
+	public void setPathWithNull() {
+		assertFalse(storage.setPath(null));
+	}
+
+	@Test
+	public void setPathWithEmptyString() {
+		assertFalse(storage.setPath(""));
+	}
+
+	@Test
+	public void setPathWithIllegalCharacters() {
+		
+		char[] illegalCharacters = new char[]{'\n', '\t', '?', '*', '<', '\\', '<', '>', '|', '\"', ':'};
+		
+		for (char c : illegalCharacters) {
+			assertFalse(storage.setPath(PATH_CURRENT + "save" + c + "file.txt"));
+		}
+	}
+
+	@Test
+	public void setPathWithoutFilename() {
+		assertFalse(storage.setPath(PATH_CURRENT));
+	}
+
+	@Test
+	public void setPathWithOnlyFilename() {
+		assertFalse(storage.setPath(FILENAME));
+	}
+	
+	@Test(expected = IOException.class)
+	public void pathPersists() throws IOException, ParseException {
+		
+		tasks.add(taskFloating);
+		
+		storage.save(tasks);
+		storage.setPath(PATH_CURRENT + "anotherSaveFile.txt");
+		
+		storage = new Storage();
+		
+		storage.load();
+	}
+
+	@After
+	public void tearDown() {
+		File file = new File(PATH_CURRENT + FILENAME);
+		file.delete();
+	}
+
+	private static void assertSameTask(Task task1, Task task2) {
+
+		assertTrue(isSame(task1.getDescription(), task2.getDescription()));
+		assertTrue(isSame(task1.getStartDate(), task2.getStartDate()));
+		assertEquals(task1.getIsComplete(), task2.getIsComplete());
+	}
+
+	// private static boolean isSameTask(Task task1, Task task2) {
+	//
+	// if (!isSame(task1.getDescription(), task2.getDescription())) {
+	// return false;
+	// }
+	//
+	// if (!isSame(task1.getStartDate(), task2.getStartDate())) {
+	// return false;
+	// }
+	//
+	// if (!isSame(task1.getdeadline(), task2.getdeadline())) {
+	// return false;
+	// }
+	//
+	// if (!isSame(task1.getVenue(), task2.getVenue())) {
+	// return false;
+	// }
+	//
+	// if (!isSame(task1.getPriority(), task2.getPriority())) {
+	// return false;
+	// }
+	//
+	// return true;
+	// }
+
+	private static boolean isSame(String string1, String string2) {
+
+		if (string1 == null && string2 == null) {
+			return true;
+		}
+
+		if (string1 != null && string1.equals(string2)) {
+			return true;
+		}
+
+		return false;
+	}
+
+	private static boolean isSame(Date date1, Date date2) {
+
+		if (date1 == null && date2 == null) {
+			return true;
+		}
+
+		if (date1 != null && date1.toString().equals(date2.toString())) {
+			return true;
+		}
+
+		return false;
+	}
+}
