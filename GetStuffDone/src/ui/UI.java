1bb4d82c4a834378443a8474cada05bb12831fbc
unyoungwax <unyoungwax@hotmail.com>
2015-10-31 23:03:11 +0800

    UI - show only recent feedbacks

--- GetStuffDone/src/ui/UI.java
+++ GetStuffDone/src/ui/UI.java
@@ -1,8 +1,9 @@
 package ui;
+
 import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
+import java.util.ArrayList;
 import javax.swing.BoxLayout;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -25,14 +26,18 @@ public class UI {
 	private static final int INFO_BOX_WIDTH = 250;
 	private static final int INFO_BOX_HEIGHT = 120;
 	private static final int SYNTAX_BOX_WIDTH = 600;
-	private static final int SYNTAX_BOX_HEIGHT = 300;
+	private static final int SYNTAX_BOX_HEIGHT = 350;
 	private static final int COMMAND_BOX_WIDTH = 200;
-	private static final int COMMAND_BOX_HEIGHT = 300;
+	private static final int COMMAND_BOX_HEIGHT = 350;
+
+	private static final int NUM_FEEDBACKS = 11;
 
 	private TextView displayBox = new TextView(DISPLAY_BOX_WIDTH, DISPLAY_BOX_HEIGHT);
 	private TextView feedbackBox = new TextView(FEEDBACK_BOX_WIDTH, FEEDBACK_BOX_HEIGHT);
 	private TextView infoBox = new TextView(INFO_BOX_WIDTH, INFO_BOX_HEIGHT);
 	private JTextField commandBar = new JTextField();
+
+	private ArrayList<String> feedbacks = new ArrayList<>(NUM_FEEDBACKS);
 
 	public static void main(String[] args) {
 		new UI();
@@ -69,11 +74,9 @@ public class UI {
 				String input = commandBar.getText();
 				commandBar.setText("");
 
-				showInFeedbackBox(input + "\n", TextView.STYLE_NORMAL);
+				addFeedback(input + "\n");
 
-				Feedback feedback = null;
-				
-					feedback = gsd.processInput(input);
+				Feedback feedback = gsd.processInput(input);
 
 				show(feedback);
 			}
@@ -94,34 +97,47 @@ public class UI {
 
 	private void show(Feedback feedback) {
 
-		showInFeedbackBox(feedback.getFeedbackString(), TextView.STYLE_FEEDBACK);
-		showInDisplayBox(feedback.getDisplayString(), TextView.STYLE_NORMAL);
-		showInInfoBox(feedback.getInfoString(), TextView.STYLE_NORMAL);
+		showInFeedbackBox(feedback.getFeedbackString());
+		showInDisplayBox(feedback.getDisplayString());
+		showInInfoBox(feedback.getInfoString());
 		showHelpBox(feedback.getHelpCommandString(), feedback.getHelpSyntaxString());
 	}
 
-	private void showInFeedbackBox(String string, String style) {
+	private void showInFeedbackBox(String string) {
 
 		if (isValidString(string)) {
-			feedbackBox.display(string, style);
+
+			addFeedback(string);
+
+			feedbackBox.clear();
+
+			for (int i = 0; i < feedbacks.size(); i++) {
+
+				if (i % 2 == 0) {
+					feedbackBox.display(feedbacks.get(i), TextView.STYLE_FEEDBACK);
+				} else {
+					feedbackBox.display(feedbacks.get(i), TextView.STYLE_NORMAL);
+				}
+			}
+
 			feedbackBox.scrollToBottom();
 		}
 	}
 
-	private void showInDisplayBox(String string, String style) {
+	private void showInDisplayBox(String string) {
 
 		if (isValidString(string)) {
 			displayBox.clear();
-			displayBox.display(string, style);
+			displayBox.display(string, TextView.STYLE_NORMAL);
 			displayBox.scrollToTop();
 		}
 	}
 
-	private void showInInfoBox(String string, String style) {
+	private void showInInfoBox(String string) {
 
 		if (isValidString(string)) {
 			infoBox.clear();
-			infoBox.display(string, style);
+			infoBox.display(string, TextView.STYLE_NORMAL);
 		}
 	}
 
@@ -137,16 +153,25 @@ public class UI {
 
 			TextView commandBox = new TextView(COMMAND_BOX_WIDTH, COMMAND_BOX_HEIGHT);
 			TextView syntaxBox = new TextView(SYNTAX_BOX_WIDTH, SYNTAX_BOX_HEIGHT);
-			
+
 			commandBox.display(commands, TextView.STYLE_NORMAL);
 			syntaxBox.display(syntax, TextView.STYLE_NORMAL);
 
 			frame.add(syntaxBox, BorderLayout.LINE_END);
 			frame.add(commandBox, BorderLayout.LINE_START);
-			
+
 			frame.pack();
 			frame.setLocationRelativeTo(null);
 			frame.setVisible(true);
+		}
+	}
+
+	private void addFeedback(String feedback) {
+
+		feedbacks.add(feedback);
+
+		if (feedbacks.size() > NUM_FEEDBACKS) {
+			feedbacks.remove(0);
 		}
 	}
 
